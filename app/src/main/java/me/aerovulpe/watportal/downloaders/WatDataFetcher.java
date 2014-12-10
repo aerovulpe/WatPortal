44a7f0f708013173bf2469543a9891652becb3fb
Aaron Nwabuoku <aerisvulpe@gmail.com>
2014-12-10 08:13:04 -0500

    Implemented WatDataFetcher as a singleton to prevent AsyncTask issues.

--- app/src/main/java/me/aerovulpe/watportal/downloaders/WatDataFetcher.java
+++ app/src/main/java/me/aerovulpe/watportal/downloaders/WatDataFetcher.java
@@ -1,10 +1,20 @@
 package me.aerovulpe.watportal.downloaders;
 
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.util.Log;
 
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 import me.aerovulpe.watportal.resources.Resource;
 import me.aerovulpe.watportal.resources.WatObject;
@@ -18,16 +28,17 @@ import static me.aerovulpe.watportal.Constants.BASE_URI;
 /**
  * Created by Aaron on 17/11/2014.
  */
-public class WatDataFetcher extends RawDataFetcher {
+public class WatDataFetcher {
     private static final WatDataFetcher INSTANCE = new WatDataFetcher();
 
     private static final String LOG_TAG = WatDataFetcher.class.getSimpleName();
 
-    private WatObjectHandler mWatObjectHandler;
+    private List<WatDataDownloader> mDataDownloaders;
 
 
     private WatDataFetcher() {
         //Restrict instantiation.
+        mDataDownloaders = new ArrayList<>();
     }
 
     public static WatDataFetcher getInstance() {
@@ -35,13 +46,16 @@ public class WatDataFetcher extends RawDataFetcher {
     }
 
     public void execute(WatObjectHandler watObjectHandler, Resource resource, String... params) {
-        mWatObjectHandler = watObjectHandler;
-        setUrl(buildCourseUri(resource, params).toString());
-        execute(new WatDataDownloader(resource));
-        Log.v(LOG_TAG, "Built URI = " + getUrl());
+        String urlStr = buildResourceUri(resource, params).toString();
+
+        WatDataDownloader dataDownloader = new WatDataDownloader(watObjectHandler, resource);
+        dataDownloader.execute(urlStr);
+
+        mDataDownloaders.add(dataDownloader);
+        Log.v(LOG_TAG, "Built URI = " + urlStr);
     }
 
-    private Uri buildCourseUri(Resource resource, String... params) {
+    private Uri buildResourceUri(Resource resource, String... params) {
         Uri.Builder uriBuilder = Uri.parse(BASE_URI).buildUpon();
         uriBuilder.appendQueryParameter(API_KEY_PARAM, API_KEY);
 
@@ -50,17 +64,25 @@ public class WatDataFetcher extends RawDataFetcher {
         return uriBuilder.build();
     }
 
-    public class WatDataDownloader extends RawDataDownloader {
+    public void killDataDownloaders() {
+        for (WatDataDownloader mDataDownloader : mDataDownloaders) {
+            mDataDownloader.cancel(true);
+        }
+    }
+
+    public class WatDataDownloader extends AsyncTask<String, Void, WatObject> {
+        private WatObjectHandler mWatObjectHandler;
         private Resource mResource;
 
-        public WatDataDownloader(Resource resource) {
+        public WatDataDownloader(WatObjectHandler watObjectHandler, Resource resource) {
+            mWatObjectHandler = watObjectHandler;
             mResource = resource;
         }
 
         @Override
-        protected Object doInBackground(String... params) {
+        protected WatObject doInBackground(String... params) {
             try {
-                return WatObject.parse(mResource, new JSONObject((String) super.doInBackground(params)));
+                return WatObject.parse(mResource, new JSONObject(getJSONString(params[0])));
             } catch (JSONException e) {
                 e.printStackTrace();
                 Log.e(LOG_TAG, "Error processing JSON data, watObject is null!", e);
@@ -70,10 +92,55 @@ public class WatDataFetcher extends RawDataFetcher {
         }
 
         @Override
-        protected void onPostExecute(Object result) {
+        protected void onPostExecute(WatObject result) {
             super.onPostExecute(result);
-            if (result instanceof WatObject) {
-                mWatObjectHandler.onWatObjectReceived((WatObject) result);
+            if (result != null) {
+                mWatObjectHandler.onWatObjectReceived(result);
+            }
+        }
+
+        private String getJSONString(String urlStr){
+            HttpURLConnection urlConnection = null;
+            BufferedReader reader = null;
+
+            if (urlStr == null)
+                return null;
+
+            try {
+                URL url = new URL(urlStr);
+
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                urlConnection.connect();
+
+                InputStream inputStream = urlConnection.getInputStream();
+
+                if (inputStream == null)
+                    return null;
+
+                StringBuffer buffer = new StringBuffer();
+
+                reader = new BufferedReader(new InputStreamReader(inputStream));
+
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    buffer.append(line + "\n");
+                }
+
+                return buffer.toString();
+            } catch (IOException e) {
+                Log.e(LOG_TAG, "Error!", e);
+                return null;
+            } finally {
+                if (urlConnection != null)
+                    urlConnection.disconnect();
+
+                if (reader != null)
+                    try {
+                        reader.close();
+                    } catch (IOException e) {
+                        Log.e(LOG_TAG, "Error closing the reader", e);
+                    }
             }
         }
     }
